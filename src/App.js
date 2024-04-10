b1eca865e6056de7915458b23021a1f0fb179321
Doyeon <tomacom0628@gmail.com>
2024-04-10 12:20:52 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -9,11 +9,24 @@ const App = () => {
 
   useEffect(() => {
     if (fileNumber) {
-      fetch(`/json/${fileNumber}_react_flow_data.json`)
-        .then((res) => res.json())
+      // 완전한 URL을 사용하여 파일 참조
+      const url = `https://Doyeon0628.github.io/suzerain-react-flow/Json/ConversationID-${fileNumber}_react_flow_data.json`;
+
+      fetch(url)
+        .then((res) => {
+          // 오류 처리를 위한 상태 코드 확인
+          if (!res.ok) {
+            throw new Error(`서버에서 파일을 찾을 수 없습니다: 상태 코드 ${res.status}`);
+          }
+          return res.json();
+        })
         .then(({ nodes, edges }) => {
           setNodes(nodes);
           setEdges(edges);
+        })
+        .catch((error) => {
+          // 오류가 발생했을 때 콘솔에 오류 메시지 출력
+          console.error("데이터를 불러오는 중 오류가 발생했습니다:", error.message);
         });
     }
   }, [fileNumber]);
